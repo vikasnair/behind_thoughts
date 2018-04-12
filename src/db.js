526cf4fa71dc64d0ceff0657ff3ecba25b1fcfe7
Vikas Nair <vikasnair@vikaaaaaaaaas.local>
2018-04-12 18:36:45 -0400

    mongodb connected via mlab

--- src/db.js
+++ src/db.js
@@ -51,4 +51,11 @@ const StrategySchema = new mongoose.Schema({
 StrategySchema.plugin(URLSlugs("title"));
 mongoose.model('User', UserSchema);
 mongoose.model('Strategy', StrategySchema);
-mongoose.connect('mongodb://heroku_g27x0hb2:Details32@ds143099.mlab.com:43099/heroku_g27x0hb2'); // TODO: make this an ENV variable
+mongoose.connect('mongodb://admin:root@ds143099.mlab.com:43099/heroku_g27x0hb2'); // TODO: make this an ENV variable
+
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));  
+mongoose.connection.once('open', () => {
+ console.log('we good');
+});
+
+ 
