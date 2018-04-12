d0cf6d09862f653f8a9218805030b2220f44275d
Vikas Nair <vikasnair@vikaaaaaaaaas.local>
2018-04-12 18:21:08 -0400

    fixing

--- src/app.js
+++ src/app.js
@@ -20,7 +20,6 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.set('port', process.env.PORT || 3000);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -157,4 +156,4 @@ app.get('/user/:username', (req, res) => {
 	});
 });
 
-const server = app.listen(app.get('port'));
+const server = app.listen(process.env.PORT || 3000);
